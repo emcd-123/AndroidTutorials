ff02e40b1873bef38c2f9e18f5da16eb85bce454
emcd-123 <emcd.unl@gmail.com>
2023-08-01 15:55:10 -0500

    2:09 in tutorial

--- app/src/main/java/edu/gatech/seclass/myapplication/MainActivity.java
+++ app/src/main/java/edu/gatech/seclass/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("fail", "it's not working");
 
+
+    }
+
+    public void handleText(View v){
+        TextView t = (TextView) findViewById(R.id.source);
+        String input = t.getText().toString();
+        System.out.println(input);
     }
 
     public void disable(View v){
-        v.setEnabled(false);
-        Log.d("Success", "Button Disabled");
+
+
+        /*v.setEnabled(false);
+        Button b = (Button) v;
+        b.setText("Disabled");
+    */
     }
+
 }
